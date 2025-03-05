d58b1bffb41c0b473acd1900c24927003d29f91c
Marco Zagaria <marcozagaria@live.com>
2025-03-05 17:30:29 +0100

    commit application properties

--- src/main/java/marco_aldo_project/expenseTracker/entities/Users.java
+++ src/main/java/marco_aldo_project/expenseTracker/entities/Users.java
@@ -1,4 +1,18 @@
 package marco_aldo_project.expenseTracker.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Users {
+    
 }
