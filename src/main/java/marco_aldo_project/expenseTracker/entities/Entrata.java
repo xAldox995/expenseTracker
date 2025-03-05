010351f137a586cfa7483c0fe623771d84ab3e0a
xAldox995 <valzanialdo@gmail.com>
2025-03-05 18:20:01 +0100

    try some fix

--- src/main/java/marco_aldo_project/expenseTracker/entities/Entrata.java
+++ src/main/java/marco_aldo_project/expenseTracker/entities/Entrata.java
@@ -1,24 +1,17 @@
 package marco_aldo_project.expenseTracker.entities;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.UUID;
+
 
 @Entity
-@Table(name = "entrate")
+@NoArgsConstructor
 @Getter
 @Setter
 public class Entrata extends Transazione {
-    @Id
-    @GeneratedValue
-    @Setter(AccessLevel.NONE)
-    private UUID id;
 
     public Entrata(double importo, String decrizione) {
         super(importo, decrizione);
--- src/main/java/marco_aldo_project/expenseTracker/entities/Transazione.java
+++ src/main/java/marco_aldo_project/expenseTracker/entities/Transazione.java
@@ -1,12 +1,22 @@
 package marco_aldo_project.expenseTracker.entities;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-
+@Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@NoArgsConstructor
+@Getter
+@Setter
 public abstract class Transazione {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
     private double importo;
     private String decrizione;
     private LocalDateTime dataTransazione;
