253290d582baebe20373d3b7af9260a72e7e5c53
Marco Zagaria <marcozagaria@live.com>
2025-03-05 18:25:31 +0100

    modify

--- src/main/java/marco_aldo_project/expenseTracker/repository/UserRepository.java
+++ src/main/java/marco_aldo_project/expenseTracker/repository/UserRepository.java
@@ -11,5 +11,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<Users, UUID> {
     Optional<Users> findByEmail(String email);
 
-    Optional<Users> findByName(String name);
+    Optional<Users> findByNome(String nome);
 }
